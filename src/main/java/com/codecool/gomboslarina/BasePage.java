5dc830adf19a110996c23408b96ded6230c4f061
gomboslarina <46283974+gomboslarina@users.noreply.github.com>
2019-11-25 10:44:27 +0100

    Revert "Glass doc permissions"

--- src/main/java/com/codecool/gomboslarina/BasePage.java
+++ src/main/java/com/codecool/gomboslarina/BasePage.java
@@ -4,35 +4,28 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.List;
-import java.util.Map;
 
 public class BasePage {
 
+    static String driverPath = System.getenv("DRIVERPATH");
+    WebDriver webDriver;
+    String url = "https://jira.codecool.codecanvas.hu/";
     String username = System.getenv("USERNAME");
     String password = System.getenv("PASSWORD");
-    private String email = System.getenv("EMAIL");
-    private String incorrectPassword = System.getenv("INCORRECT_PASSWORD");
-    private String incorrectUsername = System.getenv("INCORRECT_USERNAME");
+    String email = System.getenv("EMAIL");
+    String incorrectPassword = System.getenv("INCORRECT_PASSWORD");
+    String incorrectUsername = System.getenv("INCORRECT_USERNAME");
 
-    private static final int TIMEOUT = 10;
+    private static final int TIMEOUT = 5;
     private static final int POLLING = 100;
 
     protected WebDriver driver;
-    protected WebDriverWait wait;
-    protected FluentWait<WebDriver> fluentWait;
+    private WebDriverWait wait;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, TIMEOUT, POLLING);
-        fluentWait  = new FluentWait<>(driver)
-                .withTimeout(Duration.ofSeconds(TIMEOUT))
-                .pollingEvery(Duration.ofSeconds(POLLING))
-                .ignoring(org.openqa.selenium.ElementClickInterceptedException.class);
         PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT), this);
     }
 
@@ -44,17 +37,8 @@ public class BasePage {
         wait.until(ExpectedConditions.visibilityOf(element));
     }
 
-    protected WebElement waitForElementToBeVisible(WebElement element) {
-        wait.until(ExpectedConditions.visibilityOf(element));
-        return element;
-    }
-
     protected void waitForElementToBeClickable(WebElement element) {
         wait.until(ExpectedConditions.elementToBeClickable(element));
-    }
-
-    protected boolean isElementVisible(WebElement element) {
-        return element.isDisplayed();
     }
 
     protected void waitForElementToDisappear(WebElement element) {
@@ -83,41 +67,5 @@ public class BasePage {
 
     public String getIncorrectUsername() {
         return incorrectUsername;
-    }
-
-    public void fluentlyWait (WebElement element) {
-        fluentWait.until(ExpectedConditions.visibilityOf(element));
-    }
-
-
-    boolean areProjectAndGlassPermissionsEqual(ProjectPage projectPage, ProjectPermissionsPage projectPermissionsPage, GlassDocumentationPage glassDocumentationPage) {
-        projectPage.goToPermissions();
-        Map<String, List<Boolean>> projectPermissions = projectPermissionsPage.createMapFromProjectPagePermissions();
-        projectPage.clickOnGlassLink();
-        glassDocumentationPage.goToGlassPermissionPage();
-        Map<String, List<Boolean>> glassPermissions = glassDocumentationPage.getGlassPagePermissions();
-        int sizeOfGlassPerm = glassPermissions.keySet().size();
-        for (String projectKey : projectPermissions.keySet()) {
-            for (String glassKey : glassPermissions.keySet()) {
-                if (projectKey.equals(glassKey)) {
-                    if (compareBooleanLists(projectPermissions.get(projectKey), glassPermissions.get(glassKey))) {
-                        sizeOfGlassPerm--;
-                    } else {
-                        System.out.println("Project permission and glass permission does not equal.");
-                    }
-                }
-            }
-        }
-        return sizeOfGlassPerm == glassPermissions.keySet().size();
-    }
-
-    boolean compareBooleanLists(List<Boolean> a, List<Boolean> b) {
-        int size = a.size();
-        for (int i = 0; i < a.size(); i++) {
-            if (a.get(i) == b.get(i)) {
-                size--;
-            }
-        }
-        return size == b.size();
     }
 }
--- src/main/java/com/codecool/gomboslarina/IssuePage.java
+++ src/main/java/com/codecool/gomboslarina/IssuePage.java
@@ -1,36 +1,4 @@
 package com.codecool.gomboslarina;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
-public class IssuePage extends BasePage{
-
-    public IssuePage(WebDriver driver) {
-        super(driver);
-    }
-
-    @FindBy(xpath = "//button[@class='aui-button aui-button-primary search-button']")
-    private WebElement searchBtn;
-
-    @FindBy(xpath = "//textarea[@id='advanced-search']")
-    private WebElement searchBar;
-
-    public boolean searchForIssues(String issueName) {
-        String previousUrl = driver.getCurrentUrl();
-        waitForElementToBeClickable(searchBar);
-        searchBar.clear();
-        searchBar.sendKeys("issuekey =  " + issueName);
-        waitForElementToBeClickable(searchBtn);
-        searchBtn.click();
-        String currentUrl = driver.getCurrentUrl();
-        if (!previousUrl.equals(currentUrl)) {
-            String elementText = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".issue-link[data-issue-key='" + issueName + "']"))).getText();
-            return issueName.equals(elementText);
-        }
-        return false;
-
-    }
+public class IssuePage {
 }

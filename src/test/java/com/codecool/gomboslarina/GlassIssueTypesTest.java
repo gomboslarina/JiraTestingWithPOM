fa3b8dbadc2412e6a8475e3c8a9160f9f1a1341a
Larina Gombos <gombos.larina@gmail.com>
2019-11-28 10:40:19 +0100

    Checking IssueTypes in Glass Documentation. Comparing issuetypes found in settings with Glass page.

---
+++ src/test/java/com/codecool/gomboslarina/GlassIssueTypesTest.java
@@ -0,0 +1,28 @@
+package com.codecool.gomboslarina;
+
+import org.junit.jupiter.api.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class GlassIssueTypesTest extends BasePageTest {
+    GlassDocumentationPage glassDocumentationPage;
+    ProjectIssueTypesPage projectIssueTypesPage;
+
+    @BeforeAll
+    public void setUp() {
+        super.setUp();
+        login();
+        glassDocumentationPage = new GlassDocumentationPage(getDriver());
+        projectIssueTypesPage = new ProjectIssueTypesPage(getDriver());
+    }
+
+    @AfterAll
+    public void closeTests() {
+        super.shutDown();
+    }
+
+    @Test
+    void checkIssueTypes() {
+        Assertions.assertIterableEquals(projectIssueTypesPage.findIssueTypes(), glassDocumentationPage.findIssueTypesOnGlass());
+    }
+
+}
